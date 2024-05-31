95c9aa7261dbd1a0e0121154b02655d9a4c238d6
VanCuong2002 <cuongcoca12345@gmail.com>
2024-06-01 05:03:38 +0700

    ScrollToTop

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom'
 
 const ScrollToTop: React.FC = () => {
     const location = useLocation()
-    console.log(location.key)
 
     useEffect(() => {
         window.scrollTo({
